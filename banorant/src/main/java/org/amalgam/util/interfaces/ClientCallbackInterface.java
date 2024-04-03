381b19c6fe285421f16612729e5a45a6bd978adc
Lestat <2233915@slu.edu.ph>
2024-04-03 16:20:33 +0800

    +initial commit

---
+++ banorant/src/main/java/org/amalgam/util/interfaces/ClientCallbackInterface.java
@@ -0,0 +1,7 @@
+package org.amalgam.util.interfaces;
+
+import java.rmi.Remote;
+
+//Callback to be used for the messaging feature of the protoype
+public interface ClientCallbackInterface extends Remote {
+}
