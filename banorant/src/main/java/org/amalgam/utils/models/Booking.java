13186cbd96156a30cba2fd8afcf8d10df8dfbbab
MarvenLuis <2234244@slu.edu.ph>
2024-04-11 21:16:16 +0800

    + Made the method for fetching bookings using paymentID

--- banorant/src/main/java/org/amalgam/utils/models/Booking.java
+++ banorant/src/main/java/org/amalgam/utils/models/Booking.java
@@ -1,4 +1,53 @@
 package org.amalgam.utils.models;
 
 public class Booking {
+    private int roomID;
+    private int scheduleID;
+    private int userID;
+    private int paymentID;
+
+    public Booking() {
+        // Default constructor
+    }
+
+    public Booking(int roomID, int scheduleID, int userID, int paymentID) {
+        this.roomID = roomID;
+        this.scheduleID = scheduleID;
+        this.userID = userID;
+        this.paymentID = paymentID;
+    }
+
+    // Getters and setters
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public int getScheduleID() {
+        return scheduleID;
+    }
+
+    public void setScheduleID(int scheduleID) {
+        this.scheduleID = scheduleID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getPaymentID() {
+        return paymentID;
+    }
+
+    public void setPaymentID(int paymentID) {
+        this.paymentID = paymentID;
+    }
+
 }
