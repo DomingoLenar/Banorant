bb0a6f7feb00b104e12119adb555c072a3d0632e
Lestat <2233915@slu.edu.ph>
2024-04-23 22:15:28 +0800

    +logic to methods that handles requesting of account for deletion as well as fetching list of users that are requesting for deletion

--- banorant/src/main/java/org/amalgam/server/servicesImpl/UserServiceImpl.java
+++ banorant/src/main/java/org/amalgam/server/servicesImpl/UserServiceImpl.java
@@ -65,4 +65,67 @@ public class UserServiceImpl extends UnicastRemoteObject implements UserService,
     public List<User> getPlayers() throws RemoteException {
         return userDAL.getPlayers();
     }
+
+    //Client will call this if they want to delete their account
+    @Override
+    public boolean requestUserDeletion(String username) throws RemoteException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String path = "banorant/src/main/resources/for_deletion.json";
+        try(
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(path))
+                ){
+            JsonElement rootElement = JsonParser.parseReader(bufferedReader);
+            JsonObject rootObject = rootElement.getAsJsonObject();
+            JsonArray usersArray = rootObject.getAsJsonArray("users");
+
+            JsonObject forDeletion = new JsonObject();
+            forDeletion.addProperty("username",username);
+
+            for(JsonElement element : usersArray){
+                JsonObject currentObject = element.getAsJsonObject();
+                if(currentObject.get("username").getAsString().equals(username)){
+                    return false;
+                }
+            }
+
+            usersArray.add(forDeletion);
+
+            try(
+                    FileWriter writer = new FileWriter(path)
+                    ){
+                gson.toJson(rootElement, writer);
+                return true;
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Admin will call this to view the list of users for deletion
+    @Override
+    public LinkedList<String> fetchForDeletion() throws RemoteException {
+        LinkedList<String> listOfForDeletion = new LinkedList<>();
+        String path = "banorant/src/main/resources/for_deletion.json";
+        try(
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(path))
+                ){
+            JsonElement rootElement = JsonParser.parseReader(bufferedReader);
+            JsonObject rootObject = rootElement.getAsJsonObject();
+            JsonArray usersArray = rootObject.getAsJsonArray("users");
+            for(JsonElement element : usersArray){
+                JsonObject currentObject = element.getAsJsonObject();
+                listOfForDeletion.add(currentObject.get("username").getAsString());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return listOfForDeletion;
+    }
 }
